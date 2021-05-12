c2f3eceee659f4023d129f59bdeeacb21c0c7e37
oziel <oziel.silva222@gmail.com>
2021-05-12 17:08:31 -0300

    selecionando elementos

---
+++ Animais-fantasticos/js/script.js
@@ -0,0 +1,28 @@
+//const animais = document.getElementById('animais');
+//console.log(animais);
+
+//const gridSection = document.getElementsByClassName('grid-section');
+//console.log(gridSection[0]);
+
+//const primeiraLi = document.querySelector('li');
+//console.log(primeiraLi);
+
+//const linksInternos = document.querySelector('[href^="#"]');
+//console.log(linksInternos.href);
+
+//const animaisImg = document.querySelectorAll('img');
+//console.log(animaisImg);
+
+const imgs = document.querySelectorAll('img');
+//console.log(imgs);
+const imagens = document.querySelectorAll('img[src^="img/imagem"]');
+//console.log(imagens);
+
+const links = document.querySelectorAll('[href^="#"]');
+//console.log(links);
+
+const h2Animais = document.querySelector('.animais-descricao h2');
+//console.log(h2Animais);
+
+const paragrafos = document.querySelectorAll('p');
+console.log(paragrafos[paragrafos.length - 1]);
---
+++ DOM/script.js
@@ -0,0 +1,11 @@
+//Retorna a URL da página
+const href = window.location.href;
+console.log(href);
+
+//Retorna a classe ativo
+const elementoAtivo = document.querySelector('.ativo');
+
+//Retorna a linguagem do navegador
+const linguagem = window.navigator.language;
+console.log(linguagem);
+
