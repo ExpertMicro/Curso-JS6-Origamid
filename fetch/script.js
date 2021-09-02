9c2aebc8c4e0dffe999c0805540d1627e5b48de2
oziel <oziel.silva222@gmail.com>
2021-09-02 16:21:21 -0300

    fetch - exercicios

--- fetch/script.js
+++ fetch/script.js
@@ -53,6 +53,8 @@ cep.then(response => {
 });
 */
 
+/*
+
 const doc = fetch('./docs.txt');
 
 
@@ -63,7 +65,57 @@ doc.then(response => {
   }
 })
 
+*/
+
+/* Exercicio */
+
+const inputCep = document.getElementById('cep');
+const btnCep = document.getElementById('btnCep');
+const resultadoCep = document.querySelector('.resultadoCep');
+
+btnCep.addEventListener('click' , handleClick);
+
+function handleClick(event) {
+  event.preventDefault();
+  const cep = inputCep.value;
+  buscaCep(cep);
+}
+
+function buscaCep(cep) {
+  fetch(`https://viacep.com.br/ws/${cep}/json/`)
+  .then(response => response.text())
+  .then(body => {
+    resultadoCep.innerText = body;
+  })
+}
+
+const btcDisplay = document.querySelector('.btc');
+
+function fetchBtc() {
+  fetch('https://blockchain.info/ticker')
+  .then(response => response.json())
+  .then(btcJson => {
+    btcDisplay.innerText = ('R$ ' + btcJson.BRL.buy).replace('.', ',');
+  })
+}
+
+//setInterval(fetchBtc, 500);
+fetchBtc();
 
 
+const btnProxima = document.querySelector('.proxima');
+const paragrafoPiada = document.querySelector('.piada');
+
+function puxarPiada() {
+  fetch('https://api.chucknorris.io/jokes/random')
+  .then(r => r.json())
+  .then(piada => {
+    paragrafoPiada.innerText = piada.value;
+  })
+}
+
+btnProxima.addEventListener('click', puxarPiada);
+
+puxarPiada()
 
 
