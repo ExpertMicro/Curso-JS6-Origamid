7fa6c1d24be2223ba224b7e83f0b9d323c1677c1
oziel <oziel.silva222@gmail.com>
2021-09-05 16:51:03 -0300

    get e set

--- classes/script.js
+++ classes/script.js
@@ -17,7 +17,7 @@ console.log(blueButton);
 
 */
 
-
+/*
 class Button {
   constructor(text, background, color) {
     this.text = text;
@@ -49,7 +49,7 @@ const botaoAzul = Button.blueButton('Comprar');
 //const blueButton = new Button('Comprar', 'blue', 'white');
 
 //console.log(blueButton.appendTo('body'));
-
+*/
 
 /*
 class Button {
@@ -77,3 +77,45 @@ console.log(blueButtonStatic);
 
 */
 
+
+/*   GET E SET */
+
+const button = {
+  get tamanho() {
+    return this._numero || 100;
+  },
+  set tamanho(numero) {
+    this._numero = numero * 200;
+  }
+}
+
+const frutas = {
+  lista: [],
+  set nova(fruta) {
+    this.lista.push(fruta)
+  }
+}
+
+frutas.nova = 'Banana';
+frutas.nova = 'Morango';
+
+
+class Button {
+  constructor(text, color) {
+    this.text = text;
+    this.color = color;
+  }
+  get element() {
+    const type = this._elementType || 'button';
+    const buttonElement = document.createElement(type);
+    buttonElement.innerText = this.text;
+    buttonElement.style.color = this.color;
+    return buttonElement;
+  }
+
+  set element(type) {
+    this._elementType = type;
+  }
+}
+
+const blueButton = new Button('Comprar', 'blue');
