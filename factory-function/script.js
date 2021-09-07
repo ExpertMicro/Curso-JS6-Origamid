a22f024d7545d1bb782c81adf4ac3720a002d2c1
oziel <oziel.silva222@gmail.com>
2021-09-07 14:14:00 -0300

    factory function 2

--- factory-function/script.js
+++ factory-function/script.js
@@ -17,6 +17,7 @@ const btnBlue = createButton('Comprar');
 
 */
 
+/*
 function Pessoa(nome) {
   if(!(this instanceof Pessoa)) // ou (!new.target)
     return new Pessoa(nome);
@@ -29,3 +30,65 @@ Pessoa.prototype.andar = function() {
 
 const designer = Pessoa('Oziel');
 console.log(designer)
+
+*/
+
+
+function $$(selectedElements) {
+  const elements = document.querySelectorAll(selectedElements);
+
+  function hide() {
+    elements.forEach(element => {
+      element.style.display = 'none';
+    });
+    return this;
+  }
+
+  function show() {
+    elements.forEach(element => {
+      element.style.display = 'initial';
+    });
+    return this;
+  }
+
+  function on(onEvent, callback) {
+    elements.forEach(element => {
+      element.addEventListener(onEvent, callback);
+    });
+    return this;
+  }
+
+  function addClass(className) {
+    elements.forEach(element => {
+      element.classList.add(className);
+    });
+    return this;
+  }
+
+  function removeClass(className) {
+    elements.forEach(element => {
+      element.classList.remove(className);
+    });
+    return this;
+  }
+
+  return {
+    elements,
+    hide,
+    show,
+    on,
+    addClass,
+    removeClass,
+  }
+}
+
+const btns = $$('button');
+console.log(btns.hide().show());
+
+function handleClick(event) {
+  console.log(event.target)
+}
+
+btns.on('click', handleClick);
+btns.addClass('active')
+
