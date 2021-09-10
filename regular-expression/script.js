617875f76a9dd35387d3889d146ba585cdc8aaeb
oziel <oziel.silva222@gmail.com>
2021-09-10 15:09:49 -0300

    expressões regulares - Padrões 1

--- regular-expression/script.js
+++ regular-expression/script.js
@@ -116,7 +116,7 @@ const padraoRegexp = /a/g;
 
 const texto = 'JavaScript';
 const novoTexto = texto.replace(padraoRegexp, 'n');
-console.log(novoTexto);
+//console.log(novoTexto);
 
 const regexp = /[-.]/g; // Procura - ou .
 '111.222-333-44'.replace(regexp, ''); //11122233344
@@ -175,6 +175,72 @@ const regexooo = /(ta)+/gi;
 const regexiii = /\d(?=px)/g;
 '2em, 4px, 5%, 2px, 1px'.replace(regexiii, 'X');
 //2em, Xpx, 5%, Xpx, Xpx
+
+
+const listaFrutas = 'Melancia,banana,Morango,Uva,Jabuticaba';
+
+// transforma a listaFrutas em um array de frutas
+const arrayFrutas = listaFrutas.split(',');
+
+//Expressão regular que seleciona todas as vogais
+const vogalRegex = /[aeiou]/gi;
+let maior = 0;
+let palavraComMaisVogal = '';
+
+arrayFrutas.forEach(item => {
+      //O método match recupera o resultado da correspondência de uma string com uma expressão regular
+      const vogal = item.match(vogalRegex, "");
+      //console.log(vogal)  
+      if(vogal.length > maior) {
+            maior = vogal.length;
+            palavraComMaisVogal = `${vogal.length} ${item}`;
+      }
+      
+      
+});
+//console.log(palavraComMaisVogal);
+
+const regexCep = /\d{5}[-\s]?\d{3}/g;
+
+const ceps = [
+      '00000-000',
+      '00000 000',
+      '00000000'
+];
+
+for(cep of ceps) {
+      //console.log(cep, cep.match(regexCep));
+}
+
+const regexCpf = /(?:\d{3}[.-]?){3}\d{2}/g;
+
+const cpfs = [
+      '000.000.000-00',
+      '000-000-000-00',
+      '000.000.000.00',
+      '000000000-00',
+      '00000000000'
+];
+
+for(cpf of cpfs) {
+      //console.log(cpf, cpf.match(regexCpf));
+}
+
+
+const regexCnpj = /\d{2}[-.]?(?:\d{3}[-.]?){2}\/?\d{4}[-.]?\d{2}/g;
+
+const cnpjs = [
+      '00.000.000/0000-00',
+      '00000000000000',
+      '00-000-000-0000-00',
+      '00.000.000/000000',
+      '00.000.000.000000',
+      '00.000.000.0000.00'
+];
+
+for(cnpj of cnpjs) {
+      console.log(cnpj, cnpj.match(regexCnpj));
+}
 
 
 
