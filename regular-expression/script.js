6aee93736b5bf10d28fce16ac0d2f45f268ea934
oziel <oziel.silva222@gmail.com>
2021-09-10 16:08:44 -0300

    expressões regulares - Padrões 2

--- regular-expression/script.js
+++ regular-expression/script.js
@@ -239,8 +239,67 @@ const cnpjs = [
 ];
 
 for(cnpj of cnpjs) {
-      console.log(cnpj, cnpj.match(regexCnpj));
+      //console.log(cnpj, cnpj.match(regexCnpj));
 }
 
 
+const regexTelefone = /(?:\+?55\s?)?(?:\(?\d{2}\)?[\s-]?)?\d{4,5}[-\s]?\d{4}/;
+
+const telefones = [
+      '+55 11 98888-8888',
+      '+55 11 98888 8888',
+      '+55 11 988888888',
+      '+55 11988888888',
+      '+5511988888888',
+      '5511988888888',
+      '11 98888-8888',
+      '11 98888 8888',
+      '(11) 98888 8888',
+      '(11) 98888-8888',
+      '11-98888-8888',
+      '11 98888 8888',
+      '11988888888',
+      '988888888',
+      '(11)988888888',
+      '98888 8888',
+      '8888 8888'
+];
+
+for(telefone of telefones) {
+      //console.log(telefone, telefone.match(regexTelefone));
+}
+
+const regexEmail = /[\w.-]+@[\w-]+\.[\w.-]+/gi;
+
+const emails = [
+      'email@email.com',
+      'email@email.com.org',
+      'email-email@email.com',
+      'email_email@email.com',
+      'email.email@email.com.br',
+      'email.email123@empresa-sua.com.br',
+      'c@contato.cc',
+];
+
+for(email of emails) {
+     // console.log(email, email.match(regexEmail));
+}
+
+
+const regexTag = /<\/?[\w\s="']+\/?>/gi;
+// Seleciona apenas os nomes das tags /(?<=<\/?)[\w]+/gi
+
+const tags = [
+      '<div>Isso é uma div</div>',
+      '<div class="ativa">Essa está ativa</div>',
+      '<img src="imagem" />',
+      '<img src="imagem">',
+      '<ul class="ativa">',
+      '<li>Essa está ativa</li>',
+      '</ul>'
+];
+
+for(tag of tags) {
+      console.log(tag, tag.match(regexTag));
+}
 
