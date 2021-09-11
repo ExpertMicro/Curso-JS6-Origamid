0a41bae948b910c05494ed2979de9b238a691da3
oziel <oziel.silva222@gmail.com>
2021-09-11 18:25:59 -0300

    Expressões regulares - métodos 1

--- regular-expression/script.js
+++ regular-expression/script.js
@@ -300,6 +300,71 @@ const tags = [
 ];
 
 for(tag of tags) {
-      console.log(tag, tag.match(regexTag));
+      //console.log(tag, tag.match(regexTag));
 }
+
+// Regexp Constructor
+
+/* 
+Toda regexp é criada com o constructor RegExp() e herda as suas propriedades e métodos. Existem diferenças na sintaxe de uma Regexp criada diretamente em uma variável e de uma passada como argumento de Regexp
+*/
+
+const regexppp = /\w+/gi;
+// Se passarmos uma string, não precisamos dos //
+// e devemos utilizar \\ para meta caracteres, pois é necessário
+// escapar a \ especial. As flags são o segundo argumento
+
+const regexpObj1 = new RegExp('\\w+', 'gi');
+const regexpObj2 = new RegExp(/\w+/, 'gi');
+
+const fraseee = 'Javascript Linguagem 101';
+
+
+
+//console.log(fraseee.replace(regexppp, 'X'));
+
+
+
+// Regexp Propriedades
+// Uma regexp possui propriedades com informações sobre as flags e o conteúdo da mesma.
+
+const regexsss = /\w+/gi;
+
+regexsss.flags; // 'fi'
+regexsss.global; // true
+regexsss.ignoreCase; // true
+regexsss.multiline; // false
+regexsss.source; // '\w+'
+
+
+// regexp.test()
+
+/*
+O método test() verifica se existe ou não uma ocorrência da busca. Se existir ele retorna true. A próxima vez que chamarmos o mesmo, ele irá começar do index em que parou no último true.
+*/
+
+//console.log(regexsss.test(fraseee)) // true
+
+//let i = 1;
+while(regexsss.test(fraseee)) {
+      //console.log(i++, regexsss.lastIndex)
+}
+
+
+//regexp.exec()
+
+/*
+O exec() diferente do test(), irá retornar uma Array com mais informações do que apenas um valor booleano.
+*/
+
+const fraze = 'JavaScript, TypeScript, coffeeScript, Java';
+
+const regexsex = /\w+/g;
+
+
+let regexResult;
+while((regexResult = regexsex.exec(fraze)) !== null) {
+      console.log(regexResult[0])
+}
+
 
